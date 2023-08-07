d26c31917b938bd5e5c613110209a59790f717ad
Muddayya <muddayyaswami123@gmail.com>
2023-08-07 11:43:08 +0530

    minor package.json changes

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import { Pets } from "./components/pets/Pets";
 
 const App = () => {
-  return <Pets />;
+  // return <Pets />;
+  return <div>
+    <p>
+      hello
+    </p>
+  </div>
 };
 
 export default App;
